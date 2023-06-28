1adc563fdc3afdf1662bfe9fdd11a08b92020461
Benji Grant <benjamin.grantGRA0007@gmail.com>
2023-06-28 22:09:52 +1000

    Implement basic error checking with the submit handler

--- example/App.tsx
+++ example/App.tsx
@@ -1,22 +1,45 @@
-import { useForm } from '@stevent-team/fir'
-import { z } from 'zod'
+import { SubmitHandler, useForm } from '@stevent-team/fir'
+import { ZodIssue, z } from 'zod'
 
 // Define the structure and validation of your form
 const schema = z.object({
-  name: z.string().min(3).max(100),
-  age: z.coerce.number().min(13).optional(),
-  link: z.object({
-    label: z.string(),
-    url: z.string().url(),
-  }),
+  requiredString: z.string().trim().min(1, 'This field is required'),
+  optionalString: z.string().trim(),
+  defaultString: z.string().trim().default('Default value'),
+  number: z.coerce.number().min(3).max(10),
+  // link: z.object({
+  //   label: z.string(),
+  //   url: z.string().url(),
+  // }).default({ label: 'a', url: 'https://test.com' }),
 })
 
-const App = () => {
-  const { field } = useForm({ schema })
-  console.log(field('name'))
+const Error = ({ errors }: { errors: ZodIssue[] | undefined }) =>
+  errors && errors.length > 0 ? <span className="error">{errors.map(e => `${e.message} (${e.code})`).join(', ')}</span> : null
 
-  return <form>
-    <input type="text" />
+const App = () => {
+  const { register, handleSubmit, errors } = useForm({ schema })
+
+  const onSubmit: SubmitHandler<typeof schema> = values => {
+    console.log(values)
+  }
+
+  return <form onSubmit={handleSubmit(onSubmit)}>
+    <label htmlFor="requiredString">Required string</label>
+    <input {...register('requiredString')} type="text" />
+    <Error errors={errors?.fieldErrors.requiredString} />
+
+    <label htmlFor="optionalString">Optional string</label>
+    <input {...register('optionalString')} type="text" />
+    <Error errors={errors?.fieldErrors.optionalString} />
+
+    <label htmlFor="defaultString">Default string</label>
+    <input {...register('defaultString')} type="text" />
+    <Error errors={errors?.fieldErrors.defaultString} />
+
+    <label htmlFor="number">Number</label>
+    <input {...register('number')} type="number" />
+    <Error errors={errors?.fieldErrors.number} />
+
     <button>Save changes</button>
   </form>
 }
--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,49 @@
-import { ZodObject, ZodRawShape } from 'zod'
+import { useEffect, useRef, useState } from 'react'
+import { ZodIssue, z } from 'zod'
 
-export interface UseFormOptions<TShape extends ZodRawShape> {
-  schema: ZodObject<TShape>
+import { getDefaults } from './utils/getDefaults'
+
+export interface UseFormOptions<Schema extends z.AnyZodObject> {
+  schema: Schema
 }
 
-export const useForm = <TShape extends ZodRawShape>({ schema }: UseFormOptions<TShape>) => {
-  const field = (name: keyof TShape) => schema.shape[name]
+export type SubmitHandler<Schema extends z.AnyZodObject> = (values: z.infer<Schema>) => void
 
-  return { field }
+export const useForm = <Schema extends z.AnyZodObject>({
+  schema,
+}: UseFormOptions<Schema>) => {
+  const [formValue, setFormValue] = useState(getDefaults(schema))
+  const [errors, setErrors] = useState<z.inferFlattenedErrors<Schema, ZodIssue>>()
+
+  useEffect(() => {
+    console.log('rawValue', formValue)
+  }, [formValue])
+
+  const handleSubmit = (handler: SubmitHandler<Schema>): React.FormEventHandler<HTMLFormElement> => e => {
+    e.preventDefault()
+    const parsed = schema.safeParse(formValue)
+    if (parsed.success) {
+      handler(parsed.data)
+      setErrors(undefined)
+    } else {
+      setErrors(parsed.error.flatten(issue => issue))
+    }
+  }
+
+  const register = (name: keyof z.infer<Schema>) => {
+    const fieldRef = useRef<HTMLInputElement>(null)
+
+    if (typeof formValue[name] === 'object') {
+      throw new Error(`Field ${String(name)} is an object`)
+    }
+
+    return {
+      value: String(formValue[name] ?? ''),
+      onChange: e => setFormValue({ ...formValue, [name]: e.currentTarget.value }),
+      name: String(name),
+      ref: fieldRef,
+    } satisfies React.ComponentProps<'input'>
+  }
+
+  return { register, handleSubmit, errors }
 }
